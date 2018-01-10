9da1b20d3e5596141ed72f2f83472177788d1868
Bùi Lực <builuc1998@gmail.com>
2018-01-10 05:04:26 +0000

    new

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -34,6 +34,53 @@ export default {
             posts: [],
             errors: [],
             info:{},
+            action: '',
+        }
+    },
+    methods:{
+        install: function(){
+            this.type = [];
+            $('input[name="type[]"]:checked').each((i,e)=>{
+                this.type.push($(e).val());
+            });
+            this.package = $('#package').val();
+            this.uid = $('#uid').val();
+            this.speed = $('#speed').val();
+            this.time = $('#time').val();
+            this.action = $('#action').val();
+            if(this.action == 'like'){
+                if(this.type == ''){
+                    toastr.error("Vui lòng ch?n lo?i c?m xúc!");
+                    return false;
+                }else if(this.package == ''){
+                    toastr.error("Vui lòng ch?n gói c?m xúc!");
+                    return false;
+                }else if(this.uid == ''){
+                    toastr.error("Vui lòng nh?p UID!");
+                    return false;
+                }else if(this.speed == ''){
+                    toastr.error("Vui lòng ch?n lo?i th?i gian cron!");
+                    return false;
+                }else if(this.time == ''){
+                    toastr.error("Vui lòng ch?n gói th?i gian!");
+                    return false;
+                }
+            }
+            axios.post('api/installViplike',
+                {
+                    'uid':this.uid,
+                    'type':this.type,
+                    'package':this.package,
+                    'speed':this.speed,
+                    'time':this.time,
+                    'action':this.action
+                }).then((response) => {
+                if(response.data.success == 'true'){
+                    toastr.success(response.data.message);
+                }else{
+                    toastr.error(response.data.message);
+                }
+            })
         }
     },
     mounted() {
